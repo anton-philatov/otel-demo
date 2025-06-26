3607742448edd2ebbd6098ece22676d5adce7094
Filatov Anton <a.filatov@korona.net>
2025-06-26 09:28:39 +0300

    opentelemetry sdk

--- processor/src/main/java/ru/cft/otel/demo/Application.java
+++ processor/src/main/java/ru/cft/otel/demo/Application.java
@@ -1,5 +1,15 @@
 package ru.cft.otel.demo;
 
+import io.opentelemetry.api.GlobalOpenTelemetry;
+import io.opentelemetry.api.OpenTelemetry;
+import io.opentelemetry.api.common.AttributeKey;
+import io.opentelemetry.api.common.Attributes;
+import io.opentelemetry.api.metrics.LongCounter;
+import io.opentelemetry.api.metrics.Meter;
+import io.opentelemetry.api.trace.Span;
+import io.opentelemetry.api.trace.StatusCode;
+import io.opentelemetry.api.trace.Tracer;
+import io.opentelemetry.context.Scope;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.apache.hc.client5.http.classic.HttpClient;
@@ -14,19 +24,33 @@ import org.springframework.amqp.rabbit.annotation.RabbitListener;
 import org.springframework.boot.Banner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.stereotype.Component;
 
 import java.io.IOException;
 import java.io.UncheckedIOException;
 import java.util.List;
 
+import static io.opentelemetry.api.common.AttributeKey.stringKey;
+
 @SpringBootApplication
 public class Application {
+
+    @Configuration
+    static class OpenTelemetryConfiguration {
+        @Bean
+        public OpenTelemetry openTelemetry() {
+            return GlobalOpenTelemetry.get();
+        }
+    }
 
     @Slf4j
     @Component
     @RequiredArgsConstructor
     static class EventProcessor {
+        private static final AttributeKey<String> STATUS_KEY = stringKey("status");
+        private final OpenTelemetry openTelemetry;
         private final EventServiceHttpClient eventServiceHttpClient;
 
         @RabbitListener(queues = "events")
@@ -40,16 +64,32 @@ public class Application {
         }
 
         private boolean doProcess(String id, String body) {
-            try {
-                // do some stuff
-                if (id.hashCode() % 2 == 0) {
-                    throw new ProcessingException("Some exception occured");
+            Tracer tracer = openTelemetry.getTracer("processor");
+            Meter meter = openTelemetry.getMeter("processor");
+
+            Span span = tracer.spanBuilder("process event").startSpan();
+            LongCounter counter = meter.counterBuilder("event_processing_operation_count")
+                    .setDescription("Number of processed events")
+                    .build();
+
+            try (Scope ignore = span.makeCurrent()) {
+                try {
+                    // do some stuff
+                    if (id.hashCode() % 2 == 0) {
+                        throw new ProcessingException("Some exception occured");
+                    }
+                    counter.add(1, Attributes.of(STATUS_KEY, "succeeded"));
+                    log.info("Event processed successfully: id = {}", id);
+                    return true;
+                } catch (Exception e) {
+                    counter.add(1, Attributes.of(STATUS_KEY, "failed"));
+                    span.recordException(e);
+                    span.setStatus(StatusCode.ERROR);
+                    log.warn("Event processed with error: id = {}", id, e);
+                    return false;
                 }
-                log.info("Event processed successfully: id = {}", id);
-                return true;
-            } catch (Exception e) {
-                log.warn("Event processed with error: id = {}", id, e);
-                return false;
+            } finally {
+                span.end();
             }
         }
     }
